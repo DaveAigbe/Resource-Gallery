7d0c3d024eb7ccba8354629c533739435c84a17f
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-10-09 03:20:22 -0500

    - Added delete button to each video, non functional

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { Icon } from "@iconify/react";
 
 const Video = ({ id }) => {
   return (
-    <iframe
-      className={
-        "rounded-2xl w-full h-full md:w-330 md:h-186 my-md:w-380 my-md:h-214 lg:w-460 lg:h-259 my-lg:w-560 my-lg:h-315"
-      }
-      src={`https://www.youtube.com/embed/${id}`}
-      title="YouTube video player"
-      allowFullScreen={true}
-    ></iframe>
+    <div className={"relative"}>
+      <iframe
+        className={
+          "rounded-2xl w-full h-full md:w-330 md:h-186 my-md:w-380 my-md:h-214 lg:w-460 lg:h-259 my-lg:w-560 my-lg:h-315"
+        }
+        src={`https://www.youtube.com/embed/${id}`}
+        title="YouTube video player"
+        allowFullScreen={true}
+      ></iframe>
+      <button>
+        <Icon
+          xlinkTitle={"Delete Entry"}
+          className={
+            "absolute top-5 right-5 text-white text-2xl hover:text-red-600 transition ease-in-out duration-300"
+          }
+          icon="akar-icons:circle-x-fill"
+        />
+      </button>
+    </div>
   );
 };
 
