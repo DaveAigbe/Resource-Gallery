3aeaade40afed3b193ff5a75a2b726aa1d266899
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-10-12 00:35:25 -0500

    - Added blur when video overlay is present

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,7 @@ export const Video = ({id, handleActive}) => {
 
     return (
         <div onClick={handleActive}
-             className={'fixed bg-gray-600/25 h-screen w-screen flex justify-center items-center fixed z-10 top-0 right-0'}>
+             className={'fixed backdrop-blur-md bg-gray-600/25 h-screen w-screen flex justify-center items-center fixed z-10 top-0 right-0'}>
             <iframe
                 className={
                     'rounded-2xl md:w-330 md:h-186 my-md:w-380 my-md:h-214 lg:w-460 lg:h-259 my-lg:w-560 my-lg:h-315'
