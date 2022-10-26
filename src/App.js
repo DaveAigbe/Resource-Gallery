b597591b188152676d30d0bc404f76aba99b40bb
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-10-25 21:07:31 -0500

    - Added more hard coded categories - Updated README.md

--- src/App.js
+++ src/App.js
@@ -16,10 +16,38 @@ function App() {
         "novnyCaa7To",
         "rtR4s626ebE",
         "6OhMbf2v_jI",
+        "HvPlEJ3LHgE",
+        "QwQuro7ekvc",
+        "q_RLfOB7axQ",
+        "b42CJ0r-1to",
+        "K1iu1kXkVoA",
+        "l20MBBFZAmA",
+        "j95Lwxvi9JY",
+        "_8gHHBlbziw",
+        "DHvZLI7Db8E",
+        "2jqok-WgelI",
       ],
     },
     react: {
       ids: ["7wzMMBRVrfw", "novnyCaa7To", "rtR4s626ebE", "6OhMbf2v_jI"],
+    },
+    java: {
+      ids: [
+        "HvPlEJ3LHgE",
+        "QwQuro7ekvc",
+        "q_RLfOB7axQ",
+        "b42CJ0r-1to",
+        "K1iu1kXkVoA",
+      ],
+    },
+    node: {
+      ids: [
+        "l20MBBFZAmA",
+        "j95Lwxvi9JY",
+        "_8gHHBlbziw",
+        "DHvZLI7Db8E",
+        "2jqok-WgelI",
+      ],
     },
   });
   const [currentCategory, setCurrentCategory] = useState("all");
