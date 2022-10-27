a87ea82f7d1fa120b3da42b0fb155e66b4804b02
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-10-27 18:51:23 -0500

    - Updated categories list styling - Added UI to create new category in the categories list

--- src/components/Layout/CategoriesList.jsx
+++ src/components/Layout/CategoriesList.jsx
@@ -1,12 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../../context/Context";
 import CategoryButton from "./CategoryButton";
+import { Icon } from "@iconify/react";
+import { CategoryForm } from "./CategoryForm";
 
 export const CategoriesList = () => {
   const { videos } = useContext(Context);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleCategoryForm = () => {
+    setShowForm((prevState) => {
+      return !prevState;
+    });
+  };
 
   return (
-    <section className={"hidden sm:flex absolute top-5 text-xl gap-3"}>
+    <section className={"hidden sm:flex absolute top-5 text-xl gap-4"}>
       {Object.keys(videos).map((category) => {
         return (
           <React.Fragment key={category}>
@@ -14,6 +23,14 @@ export const CategoriesList = () => {
           </React.Fragment>
         );
       })}
+      <Icon
+        onClick={handleCategoryForm}
+        className={
+          "text-4xl text-white self-center hover:text-green-400 transition-all ease-in-out cursor-pointer"
+        }
+        icon="ant-design:plus-square-twotone"
+      />
+      {showForm && <CategoryForm />}
     </section>
   );
 };
---
+++ src/components/Layout/CategoryForm.jsx
@@ -0,0 +1,11 @@
+export const CategoryForm = () => {
+  return (
+    <form
+      className={
+        "fixed bg-purple-600 w-1/6 h-48 rounded z-10 flex justify-center"
+      }
+    >
+      <input className={"rounded h-fit p-0.5"} type="text" />
+    </form>
+  );
+};
